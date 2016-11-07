e6f4d3153ec7ac8e51e7327e794473507474a516
MateuszNakielski <mt.nakielski@gmail.com>
2016-11-07 21:50:42 +0100

    Add simple REST Controller to add product

---
+++ SklepKortowiadaWMiI/Controllers/Products1Controller.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using SklepKortowiadaWMiI.Models;
+
+namespace SklepKortowiadaWMiI.Controllers
+{
+    public class Products1Controller : ApiController
+    {
+        private SklepKortowiadaWMiIContext db = new SklepKortowiadaWMiIContext();
+
+        // GET: api/Products1
+        public IQueryable<Product> GetProducts()
+        {
+            return db.Products;
+        }
+
+        public IHttpActionResult PostProduct(Product product)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Products.Add(product);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = product.Id }, product);
+        }
+    }
+}
